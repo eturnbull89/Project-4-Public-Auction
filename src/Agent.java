e339405746d680082026816cda890810b68aead0
Tristin <tglunt@unm.edu>
2017-11-19 10:16:10 -0700

    Some comments added

--- src/Agent.java
+++ src/Agent.java
@@ -1,13 +1,23 @@
+import java.io.IOException;
+
 /**
  * Created by tristin on 11/18/2017.
  */
 public class Agent
 {
-    private String agentName;
-    private Account bankAccount;
+    private String agentName;   //to have 100% unique agent IDs I think the names would have to be made from a server
+    private Account bankAccount;    //given from a bank
 
-    public Agent()
+    /**
+     * So an agent needs to be it's own client, so it will have it's own Main method and not a
+     * constructor. The client will
+     * somehow know the hostname of the Bank server, it will tell the Bank "Hey I want info", and then
+     * the bank will return it's bankAccount. I also think that the AgentName may have to be given
+     * from the Bank(server), otherwise the agentNames can't be truly unique.
+     */
+
+    public static void main(String[] args) throws IOException
     {
-        return;
+
     }
 }
