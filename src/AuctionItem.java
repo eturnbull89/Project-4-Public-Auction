9c74beb02736173504ca3965ec2cc7a9078e18f5
Eric <eturnbull@unm.edu>
2017-11-20 17:12:01 -0700

    Initial set up for an auction item. Has fields listed in spec pdf, a constructor to set the fields, getters to get the fields and a setter to set current bid. - Eric

---
+++ src/AuctionItem.java
@@ -0,0 +1,49 @@
+import java.io.Serializable;
+
+class AuctionItem implements Serializable
+{
+    private final int auctionHouseId;
+
+    private final int itemId;
+
+    private final int minimumBid;
+
+    private int currentBid;
+
+    AuctionItem(int houseId, int id, int minimumBid)
+    {
+        this.auctionHouseId = houseId;
+
+        this.itemId = id;
+
+        this.minimumBid = minimumBid;
+
+        this.currentBid = 0;
+    }
+
+    int getAuctionHouseId()
+    {
+        return this.auctionHouseId;
+    }
+
+    int getItemId()
+    {
+        return this.itemId;
+    }
+
+    int getMinimumBid()
+    {
+        return  this.minimumBid;
+    }
+
+    int getCurrentBid()
+    {
+        return this.currentBid;
+    }
+
+    void setCurrentBid(int amount)
+    {
+        this.currentBid = amount;
+    }
+
+}
