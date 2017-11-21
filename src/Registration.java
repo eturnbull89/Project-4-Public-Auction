19ce36078483f452e3466a63873edc510211f5a7
Eric <eturnbull@unm.edu>
2017-11-21 16:07:25 -0700

    Updated Registration to include host name field. - Eric

--- src/Registration.java
+++ src/Registration.java
@@ -6,11 +6,15 @@ class Registration implements Serializable
 
     private final int houseSocket;
 
-    Registration(String name, int socket)
+    private final String houseHost;
+
+    Registration(String name, int socket, String hostName)
     {
         this.houseName = name;
 
         this.houseSocket = socket;
+
+        this.houseHost = hostName;
     }
 
     String getHouseName()
@@ -21,5 +25,10 @@ class Registration implements Serializable
     int getHouseSocket()
     {
         return this.houseSocket;
+    }
+
+    String getHostName()
+    {
+        return this.houseHost;
     }
 }
