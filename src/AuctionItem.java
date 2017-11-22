c7757568229b8dcb57f1fa653fb04b3931ce2557
Eric <eturnbull@unm.edu>
2017-11-22 00:01:16 -0700

    Added a field to hold the name of the item for sale. Is set in the constructor to a final variable and has a corresponding getter. - Eric

--- src/AuctionItem.java
+++ src/AuctionItem.java
@@ -2,8 +2,12 @@ import java.io.Serializable;
 
 class AuctionItem implements Serializable
 {
+    //Used to indicate which house the item is stored at?
     private final int auctionHouseId;
 
+    private final String name;
+
+    //Used to id the item in question
     private final int itemId;
 
     private final int minimumBid;
@@ -19,9 +23,11 @@ class AuctionItem implements Serializable
     private Integer previousBidderKey;
 
 
-    AuctionItem(int houseId, int id, int minimumBid, int key)
+    AuctionItem(int houseId, String itemName, int id, int minimumBid, int key)
     {
         this.auctionHouseId = houseId;
+
+        this.name = itemName;
 
         this.itemId = id;
 
@@ -58,6 +64,11 @@ class AuctionItem implements Serializable
         {
             this.currentBid = amount;
         }
+    }
+
+    String getName()
+    {
+        return this.name;
     }
 
 
