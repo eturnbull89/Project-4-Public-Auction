c9d9852976ff4dcc21e04aa8d52d893e99843b5b
Eric <eturnbull@unm.edu>
2017-11-23 22:56:33 -0700

    Changed the variable hostName to houseHost to indicate it is the host location for the auction house. Added a variable to store the host location of auction central, these two can be the same but it needed to be set up in a way that would allow for auction central and auction house to be at different locations. Updated the register method to use the above changes, also updated the error message to check for 5 arguments instead of 4. Also added method documentation. - Eric

--- src/AuctionHouse.java
+++ src/AuctionHouse.java
@@ -6,8 +6,8 @@ import java.util.ArrayList;
 
 public class AuctionHouse
 {
-    //Server host name
-    private String hostName;
+    //Auction house server host name.
+    private String houseHost;
 
     //Port number for this auction house
     private int housePort;
@@ -17,6 +17,9 @@ public class AuctionHouse
 
     //Port number of auction central
     private int centralPort;
+
+    //Auction central server host name.
+    private String centralHost;
 
     //Confirmation object, holds registration info set by auction central.
     private Confirmation houseReg;
@@ -30,18 +33,17 @@ public class AuctionHouse
     public static void main(String[] args) throws IOException
     {
         //Check that initial arguments number 4, if not throw an error message and exit
-        if(args.length != 4)
+        if(args.length != 5)
         {
-            System.err.println("Please retry and input the hostName, the auction house port number, auction house name, " +
-                    "and auction central port number");
+            System.err.println("Not enough or too many initial arguments");
             System.exit(1);
         }
 
         //Create a new auction house and set needed variables.
         AuctionHouse house = new AuctionHouse();
 
-        //Server host name, first argument
-        house.hostName = args[0];
+        //auction house server host name, first argument
+        house.houseHost = args[0];
 
         //This auction houses port number, second argument
         house.housePort = Integer.parseInt(args[1]);
@@ -52,20 +54,22 @@ public class AuctionHouse
         //The port number for auction central, forth argument
         house.centralPort = Integer.parseInt(args[3]);
 
+        //auction central server host name, fifth argument.
+        house.centralHost = args[4];
+
         //set up testing
         house.testingMethod();
 
         //Register with auction central and set houseReg field
-        //house.register(house.hostName, house.housePort, house.centralPort);
-
-
+        //house.register(house.houseHost, house.housePort, house.centralHost, house.centralPort);
     }
 
+    //Method used to test out some of the auction house methods and confirm if they work.
     private void testingMethod()
     {
         //Check that the server socket can be opened and print a message.
         try(
-                ServerSocket serverSocket = new ServerSocket(housePort);
+                ServerSocket serverSocket = new ServerSocket(housePort)
                 )
         {
             System.out.println("Server socket opened");
@@ -82,7 +86,7 @@ public class AuctionHouse
         ArrayList<AuctionItem> houseItems = itemLists(testCon);
 
         //Print the command line arguments to check they are being set.
-        System.out.println("hostName = "+hostName+", port = "+housePort+", house name = "+ houseName+", centralPort = "
+        System.out.println("houseHost = "+ houseHost +", port = "+housePort+", house name = "+ houseName+", centralPort = "
                            + centralPort);
 
         //Print each of the items info to check that it was created, in this case the first 3 should be paintings
@@ -104,14 +108,29 @@ public class AuctionHouse
         System.out.println("Current bid = "+houseItems.get(2).getCurrentBid());
     }
 
-    private void register(String hostName, int housePort, int centralPort) throws IOException
+    //***********************************
+    //String houseHost - The location that this auction house is located at, i.e. localhost or an ip address.
+    //int housePort - port number that this auction house is located at.
+    //String centralHost - The location that auction central is located at, i.e. localhost or an ip address.
+    //int centralPort - the port number that auction central is located at.
+    //register has no return value.
+    //register is used to register an auction house with auction central.  It starts by creating a new Registration
+    //object that contains the name of an auction house, the port number of the auction house, and the host name of
+    //the auction house. Register then tries to open a socket to auction central using centralHost and centralPort,
+    //and opening object input and output streams using the created socket. If it was able of open a socket with
+    //auction central it then sets the centralSocket variable to the created socket and writes the Registration object
+    //to auction central. It then tries to set the houseReg field to the Confirmation object that is returned by
+    //auction central. It then closes the object input and output streams. If there were any errors it prints out
+    //a message corresponding to the error.
+    //***********************************
+    private void register(String houseHost, int housePort, String centralHost, int centralPort) throws IOException
     {
         //Registration object that will be sent to auction central to register.
-        Registration centralReg = new Registration(houseName, housePort, hostName);
+        Registration centralReg = new Registration(houseName, housePort, houseHost);
 
         try(
-                //Create the socket to talk to the auction central
-                Socket centralSocket = new Socket(hostName, centralPort);
+                //Create the socket used to talk to the auction central
+                Socket centralSocket = new Socket(centralHost, centralPort);
 
                 //Create an object output stream from this auction house.
                 ObjectOutputStream outFromHouse = new ObjectOutputStream(centralSocket.getOutputStream());
@@ -128,7 +147,7 @@ public class AuctionHouse
 
             try
             {
-                //Receive back the registration object and set houseReg.
+                //Receive back the confirmation object and set houseReg.
                 this.houseReg = (Confirmation) inFromCentral.readObject();
             }
             catch (ClassNotFoundException e)
@@ -147,16 +166,27 @@ public class AuctionHouse
         }
         catch (UnknownHostException e)
         {
-            System.err.println("Don't know about host " + hostName);
+            System.err.println("Don't know about host " + centralHost);
             System.exit(1);
         }
         catch (IOException e)
         {
-            System.err.println("Couldn't get I/O for the connection to " + hostName);
+            System.err.println("Couldn't get I/O for the connection to " + centralHost);
             System.exit(1);
         }
     }
 
+    //***********************************
+    //Confirmation houseReg - Confirmation returned by auction central, contains an auction houses public id and
+    //                        its auction key.
+    //itemLists returns an array list of auction items.
+    //itemsLists is used to create the list of items each auction house has for auction.  It starts by creating an
+    //an array list of strings set to the value returned by readList(). It then starts a for loop that will add
+    //three items to the houseList array list. In the loop it starts by setting the item name to the string at an
+    //even value of listIndex and the minBid to an odd value of listIndex. It then uses these to create a new
+    //AuctionItem object and add the newly created auction item to the houseList array list. It then updates
+    //the value of listIndex by 2.
+    //***********************************
     private ArrayList<AuctionItem> itemLists(Confirmation houseReg)
     {
         //List of each houses auction items. List that will be returned.
@@ -193,18 +223,34 @@ public class AuctionHouse
         return houseList;
     }
 
+    //***********************************
+    //readList returns an array list of strings.
+    //readList is used to convert the saleList text file to an array list of strings that will be used to create
+    //auction items. It starts by creating an inputStream from the saleList text file. It then creates a buffered
+    //reader to read from the created input stream. It then loops through each line from the input stream and adds
+    //the read string to the itemList array list. Once done it closes the input stream and returns the itemList
+    //array list.
+    //***********************************
     private ArrayList<String> readList()
     {
+        //An array list of strings that will contain each items name and minimum bid amount given by the saleList
         ArrayList<String> itemList = new ArrayList<>();
 
+        //Create and inputStream using the saleList.txt file.
         InputStream input = this.getClass().getClassLoader().getResourceAsStream("saleList");
 
+        //Create a buffered reader to read the from the input stream.
         try(BufferedReader reader = new BufferedReader(new InputStreamReader(input)))
         {
+            //Loop through each line from the input stream until it is null
             for(String line; (line = reader.readLine()) != null;)
             {
+                //Add each line to the itemList
                 itemList.add(line);
             }
+
+            //Close the input stream after reading each line.
+            input.close();
         }
         catch (IOException e)
         {
