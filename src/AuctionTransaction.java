b7f75b096ce09f3802b43e6e70910dd37ecb7452
Eric <eturnbull@unm.edu>
2017-12-04 01:12:12 -0700

    Updated variables access modifiers to be private and final, included getters for the fields and updated classes that made use of the variables to use the getters instead. Added the class comment and method comments for the getters. - Eric

--- src/AuctionTransaction.java
+++ src/AuctionTransaction.java
@@ -1,10 +1,26 @@
 import java.io.Serializable;
 
+/**
+ * =============================================
+ * Project 4 Public Auction - CS 351 UNM
+ * @authors Zach Fleharty  |
+ *          Eric Turnbull  | eturnbull@unm.edu
+ *          Tristin Glunt  | tglunt@unm.edu
+ *          Adam Spanswick |
+ * =============================================
+ *
+ * AuctionTransaction is used to request holds and withdrawals on an agents account by an auction house. The bidKey
+ * field is used to store an Agents bidKey that auctionCentral keeps track of. amount is used to indicate the amount
+ * of money that needs to be placed into a hold, released from a hold, or withdrawn. request is used to indicate
+ * what kind of transaction is taking place. Each field has its own getter.
+ */
 class AuctionTransaction implements Serializable
 {
-    public Integer bidKey;
-    public int amount;
-    public int request;
+    private final Integer bidKey;
+
+    private final int amount;
+
+    private final int request;
 
     /**request tells the type of transaction this object is.
      * -1 is for placing a hold
@@ -13,7 +29,36 @@ class AuctionTransaction implements Serializable
     AuctionTransaction(Integer key, int amount, int request)
     {
         this.bidKey = key;
+
         this.amount = amount;
+
         this.request = request;
+    }
+
+    /**
+     * @return getBidKey returns an integer.
+     * getBidKey is used to get the value stored in bidKey.
+     */
+    Integer getBidKey()
+    {
+        return this.bidKey;
+    }
+
+    /**
+     * @return getAmount returns an int.
+     * getAmount is used to get the value stored in amount.
+     */
+    int getAmount()
+    {
+        return this.amount;
+    }
+
+    /**
+     * @return getRequest returns an int.
+     * getRequest is used to return the value stored in request.
+     */
+    int getRequest()
+    {
+        return this.request;
     }
 }
