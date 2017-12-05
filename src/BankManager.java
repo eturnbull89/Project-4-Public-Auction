bb025767fbe192016fd45e3941fac8f8aeec5675
Adam Spanswick <aspanswick@unm.edu>
2017-12-05 15:47:52 -0700

    Finished the Bank part of the README. Updated BankManager to accept command line args.

--- src/BankManager.java
+++ src/BankManager.java
@@ -3,7 +3,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
 
 /**
  * Adam Spanswick
@@ -12,37 +11,25 @@ import java.net.SocketException;
  */
 public class BankManager
 {
-  /**
-   * main: sets up a server connection for a agent and auction central and creates a new BankServerCommuncation object for
-   * each to proccess their requests.
-   * @param args
-   */
-  public static void main(String[] args)
-  {
-    Bank bank = new Bank();
-    int numOfArgs = 2;
-
-    if(args.length != numOfArgs)
+    /**
+     * main: sets up a server connection for a agent and auction central and creates a new BankServerCommuncation object for
+     * each to proccess their requests.
+     * @param args
+     */
+    public static void main(String[] args)
     {
-      System.out.println("Incorrect number of arguments");
-      System.exit(-1);
-    }
+        Bank bank = new Bank();
+        int numArgs = 2;
 
-    //First Command line argument is the port for Auction Central to connect to
-    int auctionCentralPort = Integer.parseInt(args[0]);
-    //Second Command line argument is the port for the Agent to connect to
-    int agentPort = Integer.parseInt(args[1]);
-
-    try
-    {
-      //Auction Central Connection
-      try
-      {
-        ServerSocket auctionCentralServerConnect = new ServerSocket(auctionCentralPort);
-        while (true)
+        if(numArgs != args.length)
         {
-          try
-          {
+            System.out.println("Incorrect number of arguments");
+        }
+
+        try
+        {
+            //Auction Central Connection
+            ServerSocket auctionCentralServerConnect = new ServerSocket(Integer.parseInt(args[0]));
             Socket clientAuctionCentral = auctionCentralServerConnect.accept();
 
             ObjectOutputStream auctionCentralOut = new ObjectOutputStream(clientAuctionCentral.getOutputStream());
@@ -51,49 +38,38 @@ public class BankManager
 
             //Handle objects from Auction Central
             BankServerCommunication commWithAuctionCentral = new BankServerCommunication(bank, clientAuctionCentral, auctionCentralOut, auctionCentralIn);
-          }
-          catch (SocketException e)
-          {
-            e.printStackTrace();
-          }
+
+            //Agent Connection
+            ServerSocket agentServerConnect = null;
+            Socket clientAgent = null;
+
+            try
+            {
+                agentServerConnect = new ServerSocket(Integer.parseInt(args[1]));
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+
+            while(true)
+            {
+                try
+                {
+                    clientAgent = agentServerConnect.accept(); //if there are new agent requests, we will create a new agent communication thread
+                }
+                catch(IOException e)
+                {
+                    e.printStackTrace();
+                }
+
+                //New thread for every agent
+                BankServerCommunication readingForAgent = new BankServerCommunication(bank, clientAgent, null, null);
+            }
         }
-      }
-      catch (IOException e)
-      {
-        System.out.println("Lost Connection to Auction Central");
-      }
-
-      //Agent Connection
-      ServerSocket agentServerConnect = null;
-      Socket clientAgent = null;
-
-      try
-      {
-        agentServerConnect = new ServerSocket(agentPort);
-      }
-      catch (IOException e)
-      {
-        e.printStackTrace();
-      }
-
-      while(true)
-      {
-        try
+        catch(IOException ee)
         {
-          clientAgent = agentServerConnect.accept(); //if there are new agent requests, we will create a new agent communication thread
+            ee.printStackTrace();
         }
-        catch(IOException e)
-        {
-          e.printStackTrace();
-        }
-
-        //New thread for every agent
-        BankServerCommunication readingForAgent = new BankServerCommunication(bank, clientAgent, null, null);
-      }
     }
-    catch(Exception e)
-    {
-      e.printStackTrace();
-    }
-  }
 }
